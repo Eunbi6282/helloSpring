25f7f70787e4ba7423b182d598a7f8e9acc48768
박은비 <eunbi628268@gmail.com>
2022-06-19 15:55:33 +0900

    [update] 0619

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class MemberServiceTest {
-  
+    MemberService memberService = new MemberService();
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    void findMembers() {
+        asserThat()
+    }
+
+    @Test
+    void findOne() {
+    }
 }
